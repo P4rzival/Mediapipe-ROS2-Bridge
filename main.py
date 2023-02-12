636a6a4f853314a435221f604994b8daa9c0e231
P4rzival <thafiz5@gmail.com>
2023-02-12 03:44:06 -0800

    TODOs added

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ pathToRobotModel = input("Path to robot model: ")
 
 startRobot(pathToRobotModel)
 
+#TODO manipulate the robot
+
+#TODO Print inputs from mediapipe
+
+#TODO Pass mediapipe inputs to ROS2
